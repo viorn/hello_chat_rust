484eb6e310a7ec596dc8d11c4685476873ff6671
Vladimir Yuyukin <viorn89@gmail.com>
2022-11-15 11:25:46 +0300

    Were added start scripts

--- start_db.sh
+++ start_db.sh
@@ -1,4 +1,2 @@
-docker run -d \
-        --name hello-chat-postgres \
-        -e POSTGRES_PASSWORD=qwerty123 \                                                
-        -p 5432:5432 postgres:alpine3.16
+#!/bin/bash
+docker run -d --name hello-chat-postgres -e POSTGRES_PASSWORD=qwerty123 -p 5432:5432 postgres:alpine3.16
